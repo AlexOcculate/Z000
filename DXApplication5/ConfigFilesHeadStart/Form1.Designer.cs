ee739e4abd1c7beea75cc2e6fa373015c5d9a642
AM O <occulate@yahoo.com>
2018-07-26 14:48:34 +1000

    DashboardDesignerHeadStart

---
+++ DXApplication5/ConfigFilesHeadStart/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ConfigFilesHeadStart
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if( disposing && (components != null) )
+         {
+            components.Dispose( );
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+         this.skinListDropDownButtonItem = new DevExpress.XtraBars.SkinDropDownButtonItem();
+         this.skinGalleryRibbonBarItem = new DevExpress.XtraBars.SkinRibbonGalleryBarItem();
+         this.skinPaletteRibbonGalleryBarItem = new DevExpress.XtraBars.SkinPaletteRibbonGalleryBarItem();
+         this.colorMixerBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+         this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+         this.skinsRibbonPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+         this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+         this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+         ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // ribbonControl1
+         // 
+         this.ribbonControl1.ExpandCollapseItem.Id = 0;
+         this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.skinListDropDownButtonItem,
+            this.skinGalleryRibbonBarItem,
+            this.skinPaletteRibbonGalleryBarItem,
+            this.colorMixerBarButtonItem});
+         this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+         this.ribbonControl1.MaxItemId = 5;
+         this.ribbonControl1.Name = "ribbonControl1";
+         this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1,
+            this.skinsRibbonPage});
+         this.ribbonControl1.Size = new System.Drawing.Size(929, 162);
+         // 
+         // skinListDropDownButtonItem
+         // 
+         this.skinListDropDownButtonItem.Id = 1;
+         this.skinListDropDownButtonItem.Name = "skinListDropDownButtonItem";
+         // 
+         // skinGalleryRibbonBarItem
+         // 
+         this.skinGalleryRibbonBarItem.Caption = "skinRibbonGalleryBarItem1";
+         this.skinGalleryRibbonBarItem.Id = 2;
+         this.skinGalleryRibbonBarItem.Name = "skinGalleryRibbonBarItem";
+         // 
+         // skinPaletteRibbonGalleryBarItem
+         // 
+         this.skinPaletteRibbonGalleryBarItem.Caption = "skinPaletteRibbonGalleryBarItem1";
+         this.skinPaletteRibbonGalleryBarItem.Id = 3;
+         this.skinPaletteRibbonGalleryBarItem.Name = "skinPaletteRibbonGalleryBarItem";
+         // 
+         // colorMixerBarButtonItem
+         // 
+         this.colorMixerBarButtonItem.Caption = "Color Mixer";
+         this.colorMixerBarButtonItem.Id = 4;
+         this.colorMixerBarButtonItem.Name = "colorMixerBarButtonItem";
+         this.colorMixerBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.colorMixerBarButtonItem_ItemClick);
+         // 
+         // ribbonPage1
+         // 
+         this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+         this.ribbonPage1.Name = "ribbonPage1";
+         this.ribbonPage1.Text = "ribbonPage1";
+         // 
+         // ribbonPageGroup1
+         // 
+         this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+         this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+         // 
+         // skinsRibbonPage
+         // 
+         this.skinsRibbonPage.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup3,
+            this.ribbonPageGroup2});
+         this.skinsRibbonPage.Name = "skinsRibbonPage";
+         this.skinsRibbonPage.Text = "Skins";
+         // 
+         // ribbonPageGroup2
+         // 
+         this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+         this.ribbonPageGroup2.Text = "ribbonPageGroup2";
+         // 
+         // ribbonPageGroup3
+         // 
+         this.ribbonPageGroup3.ItemLinks.Add(this.skinListDropDownButtonItem);
+         this.ribbonPageGroup3.ItemLinks.Add(this.skinGalleryRibbonBarItem);
+         this.ribbonPageGroup3.ItemLinks.Add(this.skinPaletteRibbonGalleryBarItem);
+         this.ribbonPageGroup3.ItemLinks.Add(this.colorMixerBarButtonItem);
+         this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+         this.ribbonPageGroup3.Text = "ribbonPageGroup3";
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(929, 517);
+         this.Controls.Add(this.ribbonControl1);
+         this.Name = "Form1";
+         this.Ribbon = this.ribbonControl1;
+         this.Text = "Form1";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+      private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+      private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+      private DevExpress.XtraBars.SkinDropDownButtonItem skinListDropDownButtonItem;
+      private DevExpress.XtraBars.Ribbon.RibbonPage skinsRibbonPage;
+      private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+      private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
+      private DevExpress.XtraBars.SkinRibbonGalleryBarItem skinGalleryRibbonBarItem;
+      private DevExpress.XtraBars.SkinPaletteRibbonGalleryBarItem skinPaletteRibbonGalleryBarItem;
+      private DevExpress.XtraBars.BarButtonItem colorMixerBarButtonItem;
+   }
+}
+
---
+++ DXApplication5/ConfigFilesHeadStart/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ConfigFilesHeadStart
+{
+   public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+   {
+      public Form1()
+      {
+         InitializeComponent( );
+         {
+            // Add reference to : System.Configuration and System.ComponentModel.DataAnnotations
+            System.Configuration.ConnectionStringSettingsCollection css = System.Configuration.ConfigurationManager.ConnectionStrings;
+         }
+      }
+
+      private void colorMixerBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         DevExpress.XtraEditors.ColorWheel.ColorWheelForm cwForm = new DevExpress.XtraEditors.ColorWheel.ColorWheelForm( );
+         cwForm.Show( );
+      }
+
+      private void Form1_Load( object sender, EventArgs e )
+      {
+         // WinForms Controls > Build an Application > Skins
+         //
+         // To register bonus skins in code, first ensure that your project references the DevExpress.BonusSkins
+         // assembly. Then call the DevExpress.UserSkins.BonusSkins.Register method before the main application
+         // form runs.
+         //
+         // private static void Main()
+         // {
+         //   DevExpress.UserSkins.BonusSkins.Register( );
+         //   DevExpress.Skins.SkinManager.EnableFormSkins( );
+         //   //
+         //   Application.EnableVisualStyles( );
+         //   Application.SetCompatibleTextRenderingDefault( false );
+         //   Application.Run( new Form1( ) );
+         // }
+
+         //TODO: App.Config!!!
+         string skinStyle = "The Bezier";
+         //string skinStyle = "Visual Studio 2013 Blue";
+         //string skinStyle = "Metropolis Dark";
+         //string skinStyle = "Office 2016 Colorful";
+         //stringt skinStyle = "Visual Studio 2013 Blue";
+         //foreach( SkinContainer cnt in SkinManager.Default.Skins )
+         //{
+         //   //( cnt.SkinName );
+         //}
+         DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle( skinStyle );
+         //
+         //for( int i = 0; i < this.skinMenuBarSubItem.ItemLinks.Count; i++ )
+         //{
+         //   if( this.skinMenuBarSubItem.ItemLinks[ i ].Caption == "The Bezier" )
+         //   {
+         //      BarButtonItem parentItem = (BarButtonItem) this.skinMenuBarSubItem.ItemLinks[ i ].Item;
+         //      parentItem.Caption = "Default Skin";
+         //      parentItem.ImageUri.Uri = "Apply";
+         //   }
+         //}
+         //
+         //TODO: this should come from a config file, e.g. App.config...
+         //this.openRecentBarListItem.MaxSubItemTextWidth = 200;
+         //this.openRecentBarListItem.ShowNumbers = true;
+         //this.openRecentBarListItem.Strings.Add( "Item1" );
+         //this.openRecentBarListItem.Strings.Add( "Item2" );
+         //this.openRecentBarListItem.Strings.Add( "Item3" );
+
+      }
+   }
+}
+//
+// [Configuration files] are used to control and manage the behavior of a [desktop and web application].
+//
+//    [Machine.config] : [machine level configuration file]
+//    This is automatically installed when you install Visual Studio.Net. Only one [Machine.config] file exists on
+//    a server. This file is at the highest level in the configuration hierarchy.
+//
+//    [App.config] : [application level configuration file]
+//    This is automatically created when you create a [Desktop application] project. This file inherits setting
+//    from the [Machine.config]
+//
+//    [Web.config] : [web application level configuration file]
+//    This is automatically created when you create an [ASP.Net web application] project. This file inherits setting
+//    from the [Machine.config]
+//
+//    Difference between [(App|Web).config] and [Machine.config]:
+//
+//    The settings made in the [(App|Web).config] file are applied to that particular desktop/web application only,
+//    whereas the settings of [Machine.config] file are applied to the ALL Desktop or whole ASP.net application.
+//
+//    [(App|Web).config] file are settings of desktop/asp.net application project level. [Machine.config] are settings
+//    of server level.
+//
+//    [(App|Web).config] will be for that particular desktop/web application whereas the [Machine.config] will for
+//    the whole machine.
+//
+//    Every Desktop/ASP.NET application that you has a [(App|Web).config] file . The settings specified in this will
+//    imply only to that application. Whereas "Your System" will have a [Machine.config] file in folder:
+//    [[ Microsoft.NET\Framework\v1.1.4322\CONFIG ]]
+//    which contains specifications and settings at a system level.
+//
+//    [Web.config] file is to override the settings from the [Machine.config] file. [Machine.config] file settings are
+//    applied to all the web applications residing on the server while [Web.config] settings are application specific.
+//
+//    [Machine.config] is configuration file for all the application in the IIS. but [Web.config] is a configuration
+//    file for a application or folder.
+//
+//    [Machine.config] for machine level configuration.but [Web.config] for a application/folder level configuration.
+//
